336b4ed11c89c451215700f166a44ec52a2daf4e
pan <robertpanvip@163.com>
2022-02-26 21:51:21 +0800

    feat: 初始化

--- demo/main.tsx
+++ demo/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import {createPortal} from 'react-dom';
 import PositionObserver from "../src";
+import {IntersectionRect} from "../src/PositionObserver";
 
 const vm = new WeakMap();
+
+
+
 
 class Test extends React.Component {
 
@@ -18,16 +22,21 @@ class Test extends React.Component {
             entries.map((entry) => {
                 vm.set(entry.target, entry)
             });
-            const entry = vm.get(this.ref.current)
+            const entry = vm.get(this.ref.current);
+
             //console.log(entry);
             this.setState({
                 entry
             })
+        },{
+            targetMargin:[50,200,0,0]
         })
         observe.observe(this.ref.current);
         setTimeout(() => {
-            this.ref.current.style.height = '1000px';
+            this.ref.current.style.height = '1250px';
         }, 2000)
+
+
 
     }
 
@@ -35,47 +44,96 @@ class Test extends React.Component {
      *
      */
     render() {
-        const {boundingClientRect, intersectionRect, intersectionRatio, isIntersecting} = this.state.entry;
+        const {boundingClientRect, intersectionRect, intersectionRatio, isIntersecting,tooltipRect} = this.state.entry;
 
         if (boundingClientRect && intersectionRect) {
-            console.log('width', intersectionRect, intersectionRect?.width);
-            console.log(intersectionRatio, isIntersecting);
+           // console.log('intersectionRect',intersectionRect);
         }
         return (
             <div style={{height: 800, width: 500, marginTop: 200, overflow: 'auto', border: '1px solid red'}}>
-                <div ref={this.ref2} style={{height: 900}}>123</div>
-                {/*<div style={{overflow: 'scroll',height:600,width:200}}>
+                <div ref={this.ref2} style={{height: 0}}>123</div>
+               {/* <div style={{overflow: 'scroll',height:600,width:200}}>
                     <div style={{height:200}}></div>
 
                 </div>*/}
-                <div id={'ww'} style={{overflow: 'scroll'}}>
-                    <div ref={this.ref}
-                         style={{overflow: 'inherit', width: 300, height: 200, marginLeft: 100, border: '1px solid'}}>
-                    123
+                <div id={'ww'} style={{overflow: 'scroll',position:'relative'}}>
+                    <div style={{height:1000}}>
+
                     </div>
+                    <div
+                        ref={this.ref}
+                        id={'ge'}
+                        style={{
+                            overflow: 'inherit',
+                            //top:0,
+                            width: 300,
+                            height: 200,
+
+                            border: '1px solid green'
+                        }}>
+                        123
+                    </div>
+
+                   {/* <div style={{
+                        overflow: 'scroll',
+                        position:'relative',
+                        marginLeft: 100,
+                        border: '1px solid red',
+                        //height: 200,
+                    }}>
+
+                        <div style={{height:50}}>
+
+                        </div>
+                        <div style={{overflow: 'auto',whiteSpace: 'nowrap'}}>
+                            <div
+                                ref={this.ref}
+                                id={'ge'}
+                                style={{
+                                    position:'absolute',
+                                    overflow: 'inherit',
+                                    //top:0,
+                                    width: 300,
+                                    height: 200,
+
+                                    border: '1px solid green'
+                                }}>
+                                123
+                            </div>
+                            <div style={{width:200,height:250,display:'inline-block'}}>
+
+                            </div>
+                        </div>
+
+                    </div>*/}
                 </div>
                 {
-                    boundingClientRect && intersectionRect && createPortal(
-                        <div style={{
-                            position: 'fixed',
-                            left: intersectionRect.x,
-                            top: intersectionRect.y,
-                            width: intersectionRect.width,
-                            height: intersectionRect.height,
-                            border: '1px solid yellow',
-                            overflow: 'hidden',
-                        }}>
-                            <div style={{
-                                width: boundingClientRect.width,
-                                height: boundingClientRect.height,
-                                border: '1px solid red'
-                            }}>
-                                456
+                    boundingClientRect && intersectionRect &&createPortal(
+                        <div
+                            style={{
+                                position: 'fixed',
+                                left: tooltipRect.left,
+                                top: tooltipRect.top,
+                                height: tooltipRect.height,
+                                width: tooltipRect.width,
+                                border: '1px solid yellow',
+                                overflow: 'hidden',
+                            }}
+                        >
+                            <div
+                                style={{
+                                    width: 200,
+                                    height: 50,
+                                    border: '1px solid red',
+                                    marginTop:boundingClientRect.top-intersectionRect.top,
+                                }}
+                            >
+                                12345678
                             </div>
                         </div>,
-                        document.body)
+                        document.body
+                    )
                 }
-
             </div>
         );
     }
--- src/PositionObserver.tsx
+++ src/PositionObserver.tsx
@@ -2,6 +2,12 @@ interface PositionObserverCallback {
     (entries: PositionObserverEntry[], observer: PositionObserver): void;
 }
 
+type TargetMargin = [number, number, number, number]
+
+interface PositionObserverInit {
+    targetMargin?: TargetMargin;
+}
+
 function getEntries(vm: Map<Element, PositionObserverEntry>) {
     const entries: PositionObserverEntry[] = [];
     vm.forEach((item, target) => {
@@ -12,7 +18,7 @@ function getEntries(vm: Map<Element, PositionObserverEntry>) {
 
 const observers = typeof WeakMap !== 'undefined' ? new WeakMap() : new Map();
 
-class IntersectionRect {
+export class IntersectionRect {
     readonly bottom: number;
     readonly height: number;
     readonly left: number;
@@ -21,14 +27,20 @@ class IntersectionRect {
     readonly width: number;
     readonly x: number;
     readonly y: number;
+    private _width?: number;
+    private _height?: number;
 
-    constructor(left: number, top: number, width: number, height: number) {
+    constructor(left: number, top: number, _width: number, _height: number) {
+        const width = _width >= 0 ? _width : 0;
+        const height = _height >= 0 ? _height : 0;
         this.left = left;
         this.top = top;
         this.right = left + width;
         this.bottom = top + height;
-        this.width = width;
-        this.height = height;
+        this.width = _width;
+        this.height = _height;
+        this._width = width;
+        this._height = height;
         this.x = left;
         this.y = top;
     }
@@ -37,6 +49,8 @@ class IntersectionRect {
         return JSON.stringify(this)
     }
 }
+
+type StrategyRect = Pick<IntersectionRect, 'left' | 'top' | 'width' | 'height'>
 
 class PositionObserverEntry {
     /** 目标元素的矩形信息 **/
@@ -47,6 +61,9 @@ class PositionObserverEntry {
     readonly intersectionRatio: number;
     /** 目标元素和视窗（根）相交的矩形信息 可以称为相交区域 **/
     intersectionRect: IntersectionRect;
+
+    tooltipRect: IntersectionRect;
+
     /** 目标元素是否可见 **/
     readonly isIntersecting: boolean;
 
@@ -71,29 +88,50 @@ class PositionObserverEntry {
     }
 }
 
+function getRelativeParentElement(target: Element) {
+    const style = getComputedStyle(target);
+    if (style.position === 'absolute') {
+        function loop(ele: Element): Element {
+            const _style = getComputedStyle(ele);
+            if (_style.position === 'relative') {
+                return ele
+            } else {
+                return loop(ele.parentNode as Element)
+            }
+        }
+
+        return loop(target.parentNode as Element)
+    } else {
+        return target.parentNode;
+    }
+}
+
+/**
+ * 计算2个窗口相交
+ * @param ViewReact
+ * @param targetRect
+ */
+function getRectByStrategy(ViewReact: StrategyRect, targetRect: StrategyRect) {
+
+    const left = Math.max(ViewReact.left, targetRect.left);
+    const top = Math.max(ViewReact.top, targetRect.top);
+    //pre https://blog.csdn.net/weixin_43395911/article/details/104955302?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_title~default-1.queryctrv2&spm=1001.2101.3001.4242.2&utm_relevant_index=4
+    const _h = Math.min(targetRect.top + targetRect.height, ViewReact.top + ViewReact.height) - Math.max(targetRect.top, ViewReact.top)
+    const _w = Math.min(targetRect.left + targetRect.width, ViewReact.left + ViewReact.width) - Math.max(targetRect.left, ViewReact.left)
+
+    return new IntersectionRect(left, top, _w, _h)
+}
+
 /**
  * 循环获取可见视口
  * @param target
  * @param targetRect
  */
 function loopGetRect(target: Element, targetRect: DOMRect): DOMRect {
-    const parent = target.parentNode as Element;
+    const parent = getRelativeParentElement(target) as Element;
     const parentRect = parent.getBoundingClientRect();
-    const ViewReact = {
-        left: parentRect.left,
-        top: parentRect.top,
-        width: parentRect.width,
-        height: parentRect.height,
-    }
-    const left = Math.max(ViewReact.left, targetRect.left);
-    const top = Math.max(ViewReact.top, targetRect.top);
-    //pre https://blog.csdn.net/weixin_43395911/article/details/104955302?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_title~default-1.queryctrv2&spm=1001.2101.3001.4242.2&utm_relevant_index=4
-    const _h = Math.min(targetRect.top + targetRect.height, ViewReact.top + ViewReact.height) - Math.max(targetRect.top, ViewReact.top)
-    const _w = Math.min(targetRect.left + targetRect.width, ViewReact.left + ViewReact.width) - Math.max(targetRect.left, ViewReact.left)
-    const width = _w >= 0 ? _w : 0;
-    const height = _h >= 0 ? _h : 0;
 
-    const intersectionRect = new IntersectionRect(left, top, width, height)
+    const intersectionRect = getRectByStrategy(parentRect, targetRect)
 
     if (!(parent.parentNode instanceof HTMLHtmlElement)) {
         return loopGetRect(parent, intersectionRect)
@@ -102,16 +140,66 @@ function loopGetRect(target: Element, targetRect: DOMRect): DOMRect {
     }
 }
 
+function getIntersectionRect(target:Element, targetRect:DOMRect) {
+    //const targetRect = target.getBoundingClientRect();
+    const left = targetRect.left
+    const top = targetRect.top
+    const width = targetRect.width
+    const height = targetRect.height;
+    const _targetRect = new IntersectionRect(left, top, width, height)
+
+    return loopGetRect(target, _targetRect);
+}
+
+function getTooltipRectRect(targetRect: DOMRect, intersectionRect: IntersectionRect, init: PositionObserverInit) {
+    const tooltipRect:any = {
+        right: intersectionRect.right + 200,
+        left: intersectionRect.right,
+        top: intersectionRect.top - 50,
+        bottom: intersectionRect.top,
+        height: 50,
+        width: 200,
+        x: intersectionRect.right,
+        y: intersectionRect.top - 50,
+    }
+    const diffTop = intersectionRect.top - targetRect.top
+    if (diffTop > 0 && diffTop <= 50) {
+        tooltipRect.height = 50 - diffTop;
+        tooltipRect.top = intersectionRect.top-50+ diffTop
+    } else if (diffTop > 50) {
+        tooltipRect.height = 0;
+        tooltipRect.top = intersectionRect.top
+    }
+
+    if (intersectionRect.height < 0 && intersectionRect.height >= -50) {
+        tooltipRect.height = 50 - Math.abs(intersectionRect.height)
+    } else if (intersectionRect.height < -50) {
+        tooltipRect.height = 0;
+        tooltipRect.top = intersectionRect.top-50
+    }
+    return tooltipRect
+}
+
 class PositionObserverSUP {
     private readonly callback: PositionObserverCallback;
     private vm = new Map<Element, PositionObserverEntry>();
     // @ts-ignore
     private resizeObserver: ResizeObserver;
+    private init: PositionObserverInit = {
+        targetMargin: [0, 0, 0, 0]
+    }
 
-    constructor(callback: PositionObserverCallback) {
+    constructor(callback: PositionObserverCallback, init: PositionObserverInit) {
         this.callback = callback;
+        this.init = init;
         // @ts-ignore
-        this.resizeObserver = ResizeObserver && new ResizeObserver(() => {
+        this.resizeObserver = ResizeObserver && new ResizeObserver((entries) => {
+            entries.forEach((entry: PositionObserverEntry) => {
+                const en = this.vm.get(entry.target);
+                const targetRect = entry.target.getBoundingClientRect();
+                en.intersectionRect = getIntersectionRect(entry.target,targetRect);
+                en.tooltipRect = getTooltipRectRect(targetRect, en.intersectionRect, this.init)
+            })
             this.callback(getEntries(this.vm), this)
         })
     }
@@ -130,10 +218,15 @@ class PositionObserverSUP {
                 const styles = getComputedStyle(item);
                 const addEventListener = (ele: Element | Window) => {
                     ele.addEventListener('scroll', () => {
-                        entry.intersectionRect = loopGetRect(target, target.getBoundingClientRect());
+
+                        const targetRect = entry.target.getBoundingClientRect();
+                        entry.intersectionRect = getIntersectionRect(target,targetRect);
+                        entry.tooltipRect = getTooltipRectRect(targetRect, entry.intersectionRect, this.init)
+
                         requestAnimationFrame(() => {
                             this.callback(getEntries(this.vm), this)
                         })
+
 
                     })
                 }
@@ -157,14 +250,14 @@ class PositionObserverSUP {
 
 
 class PositionObserver {
-    constructor(callback: PositionObserverCallback) {
+    constructor(callback: PositionObserverCallback, init?: PositionObserverInit) {
         if (!(this instanceof PositionObserver)) {
             throw new TypeError('Cannot call a class as a function.');
         }
         if (!arguments.length) {
             throw new TypeError('1 argument required, but only 0 present.');
         }
-        const controller = new PositionObserverSUP(callback);
+        const controller = new PositionObserverSUP(callback, init);
         observers.set(this, controller)
     }
 
